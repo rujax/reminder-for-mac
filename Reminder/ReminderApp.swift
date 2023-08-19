23c2d3ef95e9b441f487bd6295ca85fa6ef8ed51
chenyujia <chenyujia@innomatch.net>
2023-08-19 14:18:27 +0800

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Rujax on 2023/8/19.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
